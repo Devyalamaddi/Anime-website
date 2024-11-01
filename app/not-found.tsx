7f8b1a59b68b021e26e22fb898f77049a3c7b4ba
aquib <aquib@tringam.xyz>
2024-11-01 11:32:44 +0530

    v2

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+'use client'
+export default function NotFound() {
+    return (
+      <div className="flex min-h-screen flex-col items-center justify-center">
+        <h2 className="text-xl text-white font-bold">404 - Page Not Found</h2>
+        <p className="text-md text-white font-semibold">Sorry, we couldn't find the page you're looking for.</p>
+      </div>
+    );
+  }
